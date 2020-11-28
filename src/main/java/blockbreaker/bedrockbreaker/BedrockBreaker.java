ba7f6394a5f6681b4f54647f6a992b662f0b6e97
BuildTools <unconfigured@null.spigotmc.org>
2020-11-28 23:30:19 +0100

    Create project

---
+++ src/main/java/blockbreaker/bedrockbreaker/BedrockBreaker.java
@@ -0,0 +1,17 @@
+package blockbreaker.bedrockbreaker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BedrockBreaker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Nothing here I believe?
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Nothing here I believe?
+    }
+}
